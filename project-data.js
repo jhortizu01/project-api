2741f3944003150b53b25f86f55ed87cc351c4de
Janika Hortizuela <jhortizuela21@gmail.com>
2021-10-24 15:55:27 -0600

    Update img links

--- project-data.js
+++ project-data.js
@@ -3,7 +3,7 @@ let projects = [
     "id" : 1,
     "projectTitle": "Overlook",
     "duration": "1 Week",
-    "picture": "https://photos.app.goo.gl/JV88SAyzjPQMwtHu8",
+    "picture": "https://live.staticflickr.com/65535/51627062660_1675a24a5d_h.jpg",
     "techStack": ["Javascript", "HTML", "SCSS", "Mocha", "Chai", "Consuming Web API"], 
     "description": "The Overlook is an app that shows users past,present and future bookings for their hotel stay. Users can also book a room with their specified date and preferences.",
     "favoritePart": "This might be my favorite project to date! Definitely the cleanest code I've written. Found all of the bugs (unwanted features?) and was able to clean them up. I really pushed myself when creating this project!",
@@ -13,7 +13,7 @@ let projects = [
     "id": 2,
     "projectTitle": "Spacetagram",
     "duration": "4 Days",
-    "picture": "https://photos.app.goo.gl/JFcvDBXPModuET4S7",
+    "picture": "https://live.staticflickr.com/65535/51627062535_c6e28cfe98_h.jpg",
     "techStack": ["Javascript", "HTML", "SCSS", "Mocha", "Chai", "Webpack", "Consuming Web API"], 
     "description": "Spacetagram is an web app that displays NASA's Picture of the Day. The user can choose a date range of pictures to view. They can also like/unlike a photo and get a link of the picture to share with their friends",
     "favoritePart": "This was a fairly easy project. It was my first time creating my own Webpack (which was an adventure). Since this wasn't a project for school I really had free reign to do whatever I wanted, which was bomb.",
@@ -23,7 +23,7 @@ let projects = [
     "id": 3,
     "projectTitle": "Flashy Cards",
     "duration": "1 week",
-    "picture": "https://photos.app.goo.gl/xvNiganMHJDcC6to6",
+    "picture": "https://live.staticflickr.com/65535/51627062660_1675a24a5d_h.jpg",
     "techStack": ["Javascript"], 
     "description": "Flashy Cards is a game played through the terminal (say whaaaat?!). The user is given multiple choice questions testing the users basic javascript knowledge (that does not sound like a fun game to me 🤷🏽‍♀️)",
     "favoritePart": "This was the second time I built this project. Definitely way smoother the second time around, and I even had time to complete the extension, which was my favorite part. The extension I built was returing the time it took to complete the game. It was heavy on logic, which is one of my favorite (most days) parts about coding.",
@@ -33,7 +33,7 @@ let projects = [
     "id": 4,
     "projectTitle": "Static Comp",
     "duration": "5 Days?",
-    "picture": "https://photos.app.goo.gl/eh4FJcGn9Q88a5EAA",
+    "picture": "https://live.staticflickr.com/65535/51627062515_7ef0ba1f1e_h.jpg",
     "techStack": ["HTML", "CSS"], 
     "description": "A Static webpage based off a comp. Theme was chosen by me",
     "favoritePart": "I loooooove HTML and CSS. It was a great chance to practice my skills and it was fun picking the theme. I was really feeling myself that week.",
@@ -43,7 +43,7 @@ let projects = [
     "id": 5,
     "projectTitle": "Self Care Center",
     "duration": "1 week",
-    "picture": "https://photos.app.goo.gl/eeDfYL42wR4MQeWv6",
+    "picture": "https://live.staticflickr.com/65535/51626206751_a401de0f04_h.jpg",
     "techStack": ["Javascript", "HTML", "CSS"], 
     "description": "User logs in and chooses to see a random quote or affirmation",
     "favoritePart": "This was my first solo project. It was funny getting my hands dirty and creating something 'out of scratch'. ",
@@ -52,3 +52,4 @@ let projects = [
 ]
 
 module.exports = projects
+
