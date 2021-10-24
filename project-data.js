aec20a2a871183dfcefe51191d3da3ee724171a1
Janika Hortizuela <jhortizuela21@gmail.com>
2021-10-24 16:29:19 -0600

    added github links to data

--- project-data.js
+++ project-data.js
@@ -5,6 +5,7 @@ let projects = [
     "duration": "1 Week",
     "picture": "https://live.staticflickr.com/65535/51627062660_1675a24a5d_h.jpg",
     "techStack": ["Javascript", "HTML", "SCSS", "Mocha", "Chai", "Consuming Web API"], 
+    "github": "https://github.com/jhortizu01/Overlook",
     "description": "The Overlook is an app that shows users past,present and future bookings for their hotel stay. Users can also book a room with their specified date and preferences.",
     "favoritePart": "This might be my favorite project to date! Definitely the cleanest code I've written. Found all of the bugs (unwanted features?) and was able to clean them up. I really pushed myself when creating this project!",
     "worstPart": "Trying to figure out how to turn my true and false values from my drop down into booleans was the biggest pain in the ass and ended up being a really easy solution"
@@ -14,7 +15,8 @@ let projects = [
     "projectTitle": "Spacetagram",
     "duration": "4 Days",
     "picture": "https://live.staticflickr.com/65535/51627062535_c6e28cfe98_h.jpg",
-    "techStack": ["Javascript", "HTML", "SCSS", "Mocha", "Chai", "Webpack", "Consuming Web API"], 
+    "techStack": ["Javascript", "HTML", "SCSS", "Mocha", "Chai", "Webpack", "Consuming Web API"],
+    "github": "https://github.com/jhortizu01/Spacetagram",
     "description": "Spacetagram is an web app that displays NASA's Picture of the Day. The user can choose a date range of pictures to view. They can also like/unlike a photo and get a link of the picture to share with their friends",
     "favoritePart": "This was a fairly easy project. It was my first time creating my own Webpack (which was an adventure). Since this wasn't a project for school I really had free reign to do whatever I wanted, which was bomb.",
     "worstPart": "While learning new tech is always cool, learning how to make my own webpack was not as cool. The school that I attend has their own generic Webpack file you can use for projects. I tried to copy their webpack and figured 'this will definitely work'. It did not definitely work. After 2 days of trouble shooting (between school and being a human), I realized that I was running Webpack 5 and the school was utilizing Webpack 4. 🤦🏾‍♀️ Note to self: always check your versions and always read the docs."
@@ -25,6 +27,7 @@ let projects = [
     "duration": "1 week",
     "picture": "https://live.staticflickr.com/65535/51627062660_1675a24a5d_h.jpg",
     "techStack": ["Javascript"], 
+    "github": "https://github.com/jhortizu01/Flashy-Cards",
     "description": "Flashy Cards is a game played through the terminal (say whaaaat?!). The user is given multiple choice questions testing the users basic javascript knowledge (that does not sound like a fun game to me 🤷🏽‍♀️)",
     "favoritePart": "This was the second time I built this project. Definitely way smoother the second time around, and I even had time to complete the extension, which was my favorite part. The extension I built was returing the time it took to complete the game. It was heavy on logic, which is one of my favorite (most days) parts about coding.",
     "worstPart": "The game was played through the terminal so there was no HTML/CSS involved. BORING!!!"
@@ -35,6 +38,7 @@ let projects = [
     "duration": "5 Days?",
     "picture": "https://live.staticflickr.com/65535/51627062515_7ef0ba1f1e_h.jpg",
     "techStack": ["HTML", "CSS"], 
+    "github": "https://github.com/jhortizu01/comp",
     "description": "A Static webpage based off a comp. Theme was chosen by me",
     "favoritePart": "I loooooove HTML and CSS. It was a great chance to practice my skills and it was fun picking the theme. I was really feeling myself that week.",
     "worstPart": "Sometimes CSS is not your friend and doesn't do what you tell it to do even though you ask nicely."
@@ -45,6 +49,7 @@ let projects = [
     "duration": "1 week",
     "picture": "https://live.staticflickr.com/65535/51626206751_a401de0f04_h.jpg",
     "techStack": ["Javascript", "HTML", "CSS"], 
+    "github": "https://github.com/jhortizu01/self-care-center",
     "description": "User logs in and chooses to see a random quote or affirmation",
     "favoritePart": "This was my first solo project. It was funny getting my hands dirty and creating something 'out of scratch'. ",
     "worstPart": "No worst parts for this project for me. It was just fun. Looking at it now I might make some revisions though..."
